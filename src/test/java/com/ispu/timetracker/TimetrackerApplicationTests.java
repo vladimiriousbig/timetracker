5a03ad0e52e9b3d17d9c4d7c9bf1b070e0d4d072
Vladimir <v.orlov.wns@gmail.com>
2020-03-11 02:20:43 +0300

    Initial commit

---
+++ src/test/java/com/ispu/timetracker/TimetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispu.timetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
